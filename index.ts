e8682188d1f391bcb0aaa1e0b1bcfefc5866aedf
Half-Gray <halfgray7e@gmail.com>
2024-06-09 16:12:16 -0700

    Export newly added components

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
 import BaseLayout from './src/components/BaseLayout.astro';
+import BasicLayout from './src/components/BasicLayout.astro';
+import HomeLayout from './src/components/HomeLayout.astro';
+import HomeSiteId from './src/components/HomeSiteId.astro';
+import SiteId from './src/components/SiteId.astro';
 import SkipToMain from './src/components/SkipToMain.astro';
 
-export { BaseLayout, SkipToMain };
+export { BaseLayout, BasicLayout, HomeLayout, HomeSiteId, SiteId, SkipToMain };
