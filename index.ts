09476b22669d2620f4e950434a4a9abe3a72e8b5
Half-Gray <halfgray7e@gmail.com>
2024-06-10 02:17:07 -0700

    Rename BasicLayout to GeneralLayout
    The new name better reflects its function and avoids confusion
    with BaseLayout.

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
 import BaseLayout from './src/components/BaseLayout.astro';
-import BasicLayout from './src/components/BasicLayout.astro';
+import GeneralLayout from './src/components/GeneralLayout.astro';
 import HomeLayout from './src/components/HomeLayout.astro';
 import HomeSiteId from './src/components/HomeSiteId.astro';
 import SiteId from './src/components/SiteId.astro';
 import SkipToMain from './src/components/SkipToMain.astro';
 
-export { BaseLayout, BasicLayout, HomeLayout, HomeSiteId, SiteId, SkipToMain };
+export {
+	BaseLayout,
+	GeneralLayout,
+	HomeLayout,
+	HomeSiteId,
+	SiteId,
+	SkipToMain,
+};
