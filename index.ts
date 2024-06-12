9debe90def71522ec94d93b44bd762e0ceda44f9
Half-Gray <halfgray7e@gmail.com>
2024-06-11 22:30:07 -0700

    Export SiteInfo interface

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import BaseLayout from './src/components/BaseLayout.astro';
 import GeneralLayout from './src/components/GeneralLayout.astro';
 import HomeLayout from './src/components/HomeLayout.astro';
 import HomeSiteId from './src/components/HomeSiteId.astro';
+import { type SiteInfo } from './src/site-info';
 import SiteId from './src/components/SiteId.astro';
 import SkipToMain from './src/components/SkipToMain.astro';
 
@@ -13,5 +14,6 @@ export {
 	HomeLayout,
 	HomeSiteId,
 	SiteId,
+	type SiteInfo,
 	SkipToMain,
 };
