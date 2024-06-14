35111ce32409a373ca038f309ca46e93ce3a3fab
Half-Gray <halfgray7e@gmail.com>
2024-06-14 01:37:44 -0700

    Move virtual module type declaration to separate file

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,1 @@
 /// <reference types="astro/client" />
-declare module 'virtual:grayspace/site-info' {
-	import { SiteInfo } from './site-info';
-	const siteInfo: SiteInfo;
-	export default siteInfo;
-}
---
+++ src/virtual.d.ts
@@ -0,0 +1,5 @@
+declare module 'virtual:grayspace/site-info' {
+	import { SiteInfo } from './site-info';
+	const siteInfo: SiteInfo;
+	export default siteInfo;
+}
