fcbc9a2f6255096f9f9ef77dd8a5e37a59d62086
Half-Gray <halfgray7e@gmail.com>
2024-06-16 02:19:37 -0700

    Export TextDirection type

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import { integration, type GrayspaceOptions } from './src/integration';
+import { type TextDirection } from './src/i18n';
 
 export default integration;
-export { type GrayspaceOptions };
+export { type GrayspaceOptions, type TextDirection };
