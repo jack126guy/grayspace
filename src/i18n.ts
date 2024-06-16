c640f18eec48d1046b767bdb9cea853231956da8
Half-Gray <halfgray7e@gmail.com>
2024-06-15 19:06:13 -0700

    Rename i18n function to translate to better convey function

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 import y18n, { type Y18N } from 'y18n';
 import { resolveRelativePath } from './fs';
 
-export function i18n(locale?: string): Y18N {
+export function translate(locale?: string): Y18N {
 	return y18n({
 		directory: resolveRelativePath('./locales', import.meta.url),
 		updateFiles: false,
