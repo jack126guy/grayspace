b04ab669447b0435487cd215c5cc6c8c3116b899
Half-Gray <halfgray7e@gmail.com>
2024-06-15 19:21:18 -0700

    Add support for text direction (RTL scripts)

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,10 +1,35 @@
 import y18n, { type Y18N } from 'y18n';
 import { resolveRelativePath } from './fs';
 
+export type TextDirection = 'ltr' | 'rtl';
+
 export function translate(locale?: string): Y18N {
 	return y18n({
 		directory: resolveRelativePath('./locales', import.meta.url),
 		updateFiles: false,
 		locale,
 	});
+}
+
+export function getTextDirection(locale?: string): TextDirection | null {
+	if (!locale) {
+		return null;
+	}
+	const intlLocale = new Intl.Locale(locale) as LocaleWithTextInfo;
+	if (intlLocale.getTextInfo) {
+		return intlLocale.getTextInfo().direction;
+	}
+	return intlLocale.textInfo?.direction || null;
+}
+
+// Type definitions based on locale info proposal
+// https://tc39.es/proposal-intl-locale-info/
+// Note that the properties were changed to getters in January 2023
+interface LocaleWithTextInfo extends Intl.Locale {
+	getTextInfo?(): TextInfo;
+	textInfo?: TextInfo;
+}
+
+interface TextInfo {
+	direction: TextDirection;
 }
