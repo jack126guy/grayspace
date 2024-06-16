94579ac9fb8e590b7d75a9bfab3eaa366a3b48cb
Half-Gray <halfgray7e@gmail.com>
2024-06-15 17:01:31 -0700

    Fix locale file lookup
    Locale files should be relative to the module, not the working directory
    of execution.

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,5 @@
 import y18n, { type Y18N } from 'y18n';
 
 export function i18n(locale?: string): Y18N {
-	return y18n({ directory: './src/locales', locale });
+	return y18n({ directory: import.meta.dirname + '/locales', locale });
 }
