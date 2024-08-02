c54ac9c3cf2d1f077cdf7fad2f37d6094a8f9220
Half-Gray <halfgray7e@gmail.com>
2024-08-02 16:27:38 -0700

    Expand test for homepage

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,5 +1,31 @@
 describe('home page', () => {
-	it('loads', () => {
+	beforeEach(() => {
 		cy.visit('/');
+	});
+	it('has skip to main link', () => {
+		cy.get('header .skip-to-main')
+			.should('have.length', 1)
+			.and('have.attr', 'href', '#main');
+	});
+	it('has home site ID', () => {
+		cy.get('header .home-site-id').as('home-site-id');
+
+		cy.get('@home-site-id').should('have.length', 1);
+		cy.get('@home-site-id')
+			.find('.home-site-logo')
+			.should('exist')
+			.find('img')
+			.should('have.attr', 'src', '/logo.png');
+		cy.get('@home-site-id')
+			.find('h1')
+			.should('exist')
+			.and('have.text', 'Grayspace Demo');
+	});
+	it('has main content', () => {
+		cy.get('main')
+			.should('have.length', 1)
+			.and('have.attr', 'id', 'main')
+			.find('p')
+			.should('exist');
 	});
 });
