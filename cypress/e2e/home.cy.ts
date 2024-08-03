2ef6d21ffad27b0e04003084011adab24edda730
Half-Gray <halfgray7e@gmail.com>
2024-08-02 23:39:11 -0700

    Combine homepage tests
    Cypress recommends fewer, larger tests when performing E2E testing:
    https://docs.cypress.io/guides/references/best-practices
    
    These tests don't need to be isolated from each other so they can
    be combined.

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -2,14 +2,12 @@ describe('home page', () => {
 	beforeEach(() => {
 		cy.visit('/');
 	});
-	it('has skip to main link', () => {
+	it('has contents', () => {
 		cy.get('header .skip-to-main')
 			.should('have.length', 1)
 			.and('have.attr', 'href', '#main');
-	});
-	it('has home site ID', () => {
-		cy.get('header .home-site-id').as('home-site-id');
 
+		cy.get('header .home-site-id').as('home-site-id');
 		cy.get('@home-site-id').should('have.length', 1);
 		cy.get('@home-site-id')
 			.find('.home-site-logo')
@@ -20,8 +18,7 @@ describe('home page', () => {
 			.find('h1')
 			.should('exist')
 			.and('have.text', 'Grayspace Demo');
-	});
-	it('has main content', () => {
+
 		cy.get('main')
 			.should('have.length', 1)
 			.and('have.attr', 'id', 'main')
