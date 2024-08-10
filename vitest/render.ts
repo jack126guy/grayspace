7c02674e77fb3524a40364e96a11c914d7df8c28
Half-Gray <halfgray7e@gmail.com>
2024-08-09 19:11:47 -0700

    Add separate test render function for fragments
    * Factor out Astro Container API rendering to string
    * Don't alias load function from cheerio

--- vitest/render.ts
+++ vitest/render.ts
@@ -1,16 +1,28 @@
 import { experimental_AstroContainer as AstroContainer } from 'astro/container';
 import { type AstroComponentFactory } from 'astro/runtime/server/index.js';
-import { load as loadDocument, type CheerioAPI } from 'cheerio';
+import { load, type CheerioAPI } from 'cheerio';
+
+async function renderComponent(
+	component: AstroComponentFactory,
+	props?: Record<string, unknown>,
+	slots?: Record<string, unknown>
+): Promise<string> {
+	const container = await AstroContainer.create();
+	return await container.renderToString(component, { props, slots });
+}
 
 export async function renderToDocument(
 	component: AstroComponentFactory,
 	props?: Record<string, unknown>,
 	slots?: Record<string, unknown>
 ): Promise<CheerioAPI> {
-	const container = await AstroContainer.create();
-	const rendered = await container.renderToString(component, {
-		props,
-		slots,
-	});
-	return loadDocument(rendered);
+	return load(await renderComponent(component, props, slots));
+}
+
+export async function renderToFragment(
+	component: AstroComponentFactory,
+	props?: Record<string, unknown>,
+	slots?: Record<string, unknown>
+): Promise<CheerioAPI> {
+	return load(await renderComponent(component, props, slots), null, false);
 }
