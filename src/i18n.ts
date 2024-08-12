68e444fc4555c4faf07dabe46818ca52646963b4
Half-Gray <halfgray7e@gmail.com>
2024-08-11 22:13:36 -0700

    Make text direction a translatable string
    This simplifies the design and allows adding new locales more easily.

--- src/i18n.ts
+++ src/i18n.ts
@@ -10,26 +10,3 @@ export function translate(locale?: string): Y18N {
 		locale,
 	});
 }
-
-export function getTextDirection(locale?: string): TextDirection | null {
-	if (!locale) {
-		return null;
-	}
-	const intlLocale = new Intl.Locale(locale) as LocaleWithTextInfo;
-	if (intlLocale.getTextInfo) {
-		return intlLocale.getTextInfo().direction;
-	}
-	return intlLocale.textInfo?.direction || null;
-}
-
-// Type definitions based on locale info proposal
-// https://tc39.es/proposal-intl-locale-info/
-// Note that the properties were changed to getters in January 2023
-interface LocaleWithTextInfo extends Intl.Locale {
-	getTextInfo?(): TextInfo;
-	textInfo?: TextInfo;
-}
-
-interface TextInfo {
-	direction: TextDirection;
-}
