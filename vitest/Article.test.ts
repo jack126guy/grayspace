1096eccbbeba41328e4fe534f3ad46fa7cca914a
Half-Gray <halfgray7e@gmail.com>
2025-05-08 03:19:34 -0700

    Fix unit test for Article and GeneralHeader (take data from locals)

--- vitest/Article.test.ts
+++ vitest/Article.test.ts
@@ -4,26 +4,32 @@ import { describe, it, expect } from 'vitest';
 
 describe('Article', () => {
 	it('renders article element', async () => {
-		const fragment = await renderToFragment(Article);
+		const localData = { page: {} };
+
+		const fragment = await renderToFragment(Article, { localData });
 
 		const rootElement = fragment(':root');
 		expect(rootElement.prop('tagName')).to.equal('ARTICLE');
 	});
 
 	it('renders title', async () => {
-		const props = {
-			title: 'Test Article',
+		const localData = {
+			page: {
+				title: 'Test Article',
+			},
 		};
 
-		const fragment = await renderToFragment(Article, { props });
+		const fragment = await renderToFragment(Article, { localData });
 
 		const titleHeading = fragment('article h1');
 		expect(titleHeading).to.be.unique;
-		expect(titleHeading.text()).to.equal(props.title);
+		expect(titleHeading.text()).to.equal(localData.page.title);
 	});
 
 	it('does not render title without prop', async () => {
-		const fragment = await renderToFragment(Article);
+		const localData = { page: {} };
+
+		const fragment = await renderToFragment(Article, { localData });
 
 		const titleHeading = fragment('article h1');
 		expect(titleHeading).to.have.lengthOf(0);
@@ -33,8 +39,9 @@ describe('Article', () => {
 		const slots = {
 			default: 'Test content',
 		};
+		const localData = { page: {} };
 
-		const fragment = await renderToFragment(Article, { slots });
+		const fragment = await renderToFragment(Article, { slots, localData });
 
 		expect(fragment('article').text()).to.contain(slots.default);
 	});
--- vitest/GeneralHeader.test.ts
+++ vitest/GeneralHeader.test.ts
@@ -4,48 +4,58 @@ import { describe, it, expect } from 'vitest';
 
 describe('GeneralHeader', () => {
 	it('renders container', async () => {
-		const fragment = await renderToFragment(GeneralHeader);
+		const localData = { site: {} };
+
+		const fragment = await renderToFragment(GeneralHeader, { localData });
 
 		const container = fragment(':root');
 		expect(container.prop('tagName')).to.equal('DIV');
 	});
 
 	it('renders home link', async () => {
-		const props = {
-			homeLink: '/',
-		}
+		const localData = {
+			site: {
+				homeLink: '/',
+			},
+		};
 
-		const fragment = await renderToFragment(GeneralHeader, { props });
+		const fragment = await renderToFragment(GeneralHeader, { localData });
 
 		const homeLinkElement = fragment('.site-id a');
-		expect(homeLinkElement.attr('href')).to.equal(props.homeLink);
+		expect(homeLinkElement.attr('href')).to.equal(localData.site.homeLink);
 	});
 
 	it('renders site logo', async () => {
-		const props = {
-			siteLogo: '/logo.png',
+		const localData = {
+			site: {
+				siteLogo: '/logo.png',
+			},
 		};
 
-		const fragment = await renderToFragment(GeneralHeader, { props });
+		const fragment = await renderToFragment(GeneralHeader, { localData });
 
 		const siteLogoImage = fragment('.site-logo img');
-		expect(siteLogoImage.attr('src')).to.equal(props.siteLogo);
+		expect(siteLogoImage.attr('src')).to.equal(localData.site.siteLogo);
 	});
 
-	it('does not render site logo without prop', async () => {
-		const fragment = await renderToFragment(GeneralHeader);
+	it('does not render site logo without local data', async () => {
+		const localData = { site: {} };
+
+		const fragment = await renderToFragment(GeneralHeader, { localData });
 
 		const siteLogoContainer = fragment('.site-logo');
 		expect(siteLogoContainer).to.have.lengthOf(0);
 	});
 
 	it('renders site name', async () => {
-		const props = {
-			siteName: 'Test Site',
-		}
+		const localData = {
+			site: {
+				siteName: 'Test Site',
+			},
+		};
 
-		const fragment = await renderToFragment(GeneralHeader, { props });
+		const fragment = await renderToFragment(GeneralHeader, { localData });
 
-		expect(fragment('.site-id').text()).to.contain(props.siteName);
+		expect(fragment('.site-id').text()).to.contain(localData.site.siteName);
 	});
 });
